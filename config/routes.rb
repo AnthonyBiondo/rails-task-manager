ea5a7e5fa3eb7c542142c20c8b510072e38ba9bc
Biondo Anthony <anthony.biondo@kedgebs.com>
2024-03-05 15:08:18 +0100

    optio done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  # get 'tasks', to: 'tasks#index'
 
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
   # Defines the root path route ("/")
   # root "posts#index"
 end
